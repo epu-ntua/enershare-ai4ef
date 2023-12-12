da84ee34c3fd39e0f44306441fa801e9fd780856
Alex Tzortzis <39265932+TzorAL@users.noreply.github.com>
2023-12-12 17:36:34 +0200

    Update swagger-initializer.js

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "/docs/ai4ef_service.json",
+    url: "docs/ai4ef_service.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
